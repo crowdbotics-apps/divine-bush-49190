ff187edde11e937ef03309cab5f1a907e0aedc09
Crowdbotics <team@crowdbotics.com>
2024-08-28 12:23:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-bush-49190",
+  url: "https://divine-bush-49190.botics.co",
+  api: "https://divine-bush-49190.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
